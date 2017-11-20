79bd94238f1ea4f4f4e9acb984542b91572de5a7
nickhartjenstein <hartjenstein@Hartjensteins.local>
2017-11-20 19:53:09 +0100

    *

--- examples/higher_order/main.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import HelloMessage from './HelloMessage';
-
-var mountNode = document.getElementById('examplesMountPoint');
-ReactDOM.render(<HelloMessage model="Hello" log={true}/>, mountNode);
--- steps/Anhang_Build-Prozess/99_React-Hot-Loader/src/client/components/NoMatch.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function NoMatch() {
-  return <div>Page not found</div>;
-}
